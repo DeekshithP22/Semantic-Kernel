e42f6bcbd1688ab20fd29a4d51a91e832d43e2ec
DeekshithP22 <152277265+DeekshithP22@users.noreply.github.com>
2024-03-11 19:50:35 +0530

    Update semantic.py

--- semantic.py
+++ semantic.py
@@ -140,3 +140,67 @@ Chat_complete = kernel.register_semantic_function(
 )
 response = await Chat_complete.invoke_async(context[0].text, question)
 print(response)
+
+
+
+Text: 7                                                                                         www.visionias.in                                                                                    ©Vision IAS  Conclusion  
+As the government explores the idea of simultaneous elections further, a thorough and transparent deliberation should 
+guide any decisions to ensure that the integrity of our democratic process remains intact.  
+1.3. ARTIFICIAL INTE LLIGENCE AND ELECTIO NS 
+Why in the news?  
+Five Swiss political parties have signed a code of conduct agreeing to limit the 
+use of artificial intelligence (AI) in their campaigns for the federal elections.  
+Role of AI in elections  
+• Predictive Modelling: The prediction models can be built using AI. It will help determine the probability of a voter 
+backing a specific candidate.  
+o Factors such as demographics, voting patterns and issues that matter can be considered to build a model that  
+helps identify the voters who will vote for a particular  candidate.  
+• Social Media Analysis: Using AI, parties can analyze the trends and sentiment to build a social media outreach 
+program, as per the voters’ preferences.     
+• Personalization:  AI can aid in tailoring communications for specific voters based on their interests, preferences, and 
+traits. This can assist candidates in connecting with the audience  more deeply.  
+• Real -Time Data Analysis:  Campaigns may adapt their messaging and outreach methods in reaction to shifting 
+conditions by using AI to evaluate real -time data on social media trends, sentiment, and influencers . 
+• Increased Efficiency:  AI can track and measure the performance of variou s campaign activities such as advertising, 
+canvassing, and events. This enables campaigns to identify what works and doesn’t and make data -driven decisions 
+to optimize their activities.  
+• Enhance participation: AI also generates chances for participative dem ocracy, like enhanced voter education and 
+mobilisation.  
+Concerns Associated with AI in Elections  
+• Manipulation:  AI can be used to create deepfake videos, misinformation, and social media manipulation, influencing 
+Relevance:0.8626366806471818
+
+<semantic_kernel.kernel.Kernel at 0x19c3734cb50>
+<semantic_kernel.kernel.Kernel at 0x19c3734cb50>
+142
+---------------------------------------------------------------------------
+ValidationError                           Traceback (most recent call last)
+Cell In[30], line 1
+----> 1 response = await Chat_complete.invoke_async(context[0].text, question)
+
+File c:\Users\deekshith.p\Semantic_Kernel_VScode\.venv\lib\site-packages\semantic_kernel\orchestration\sk_function.py:389, in SKFunction.invoke_async(self, input, variables, context, memory, settings, log)
+    386 from semantic_kernel.orchestration.sk_context import SKContext
+    388 if context is None:
+--> 389     context = SKContext(
+    390         variables=ContextVariables("") if variables is None else variables,
+    391         skill_collection=self._skill_collection,
+    392         memory=memory if memory is not None else NullMemory.instance,
+    393         logger=log if log is not None else self._log,
+    394     )
+    395 else:
+    396     # If context is passed, we need to merge the variables
+    397     if variables is not None:
+
+File c:\Users\deekshith.p\Semantic_Kernel_VScode\.venv\lib\site-packages\semantic_kernel\orchestration\sk_context.py:59, in SKContext.__init__(self, variables, memory, skill_collection, logger)
+     56 if skill_collection is None:
+     57     skill_collection = ReadOnlySkillCollection()
+---> 59 super().__init__(
+     60     variables=variables, memory=memory, skill_collection=skill_collection
+     61 )
+     62 self._logger = logger or NullLogger()
+
+File c:\Users\deekshith.p\Semantic_Kernel_VScode\.venv\lib\site-packages\pydantic\main.py:341, in pydantic.main.BaseModel.__init__()
+
+ValidationError: 1 validation error for SKContext
+variables
+  value is not a valid dict (type=type_error.dict)
